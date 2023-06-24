031874f681925629bf6c0960b06938c4fcbbed1a
NikiciaDev <nikiciayt@gmail.com>
2023-06-24 22:01:53 +0200

    test

--- AutoClicker.cpp
+++
@@ -1,9 +0,0 @@
-#include "AutoClicker.h"
-
-AutoClicker::AutoClicker(const std::string name, const Category category) : Module(name, category)  {
-	
-}
-
-void AutoClicker::on_call() {
-
-}
--- Color.cpp
+++
@@ -1,3 +0,0 @@
-#include "Color.h"
-
-Color::Color(unsigned char red, unsigned char green, unsigned char blue) : red(red), green(green), blue(blue) { }
