c89e8d4a8b8031e31e5acb99dc422f04d0abac84
NikiciaDev <nikiciayt@gmail.com>
2023-06-25 11:38:49 +0200

    added base colors fix

--- Whiteout.cpp
+++ Whiteout.cpp
@@ -4,7 +4,7 @@ std::string Whiteout::name = "Whiteout";
 std::string Whiteout::build = "b0.0";
 std::string Whiteout::name_build = "Whiteout | b0.0";
 sf::Color Whiteout::bg_color = sf::Color(20, 20, 20);
-sf::Color Whiteout::bg_color = sf::Color(110, 69, 56);
+sf::Color Whiteout::base_color = sf::Color(110, 69, 56);
 sf::Color Whiteout::sm_base_color = sf::Color(255, 255, 255);
 
 Whiteout::Whiteout(sf::RenderWindow* window) { 
