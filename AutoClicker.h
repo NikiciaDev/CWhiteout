af0f660accb6e43d77137aa69cc167d2c4519802
NikiciaDev <nikiciayt@gmail.com>
2023-06-26 08:04:26 +0200

    added increment to NumberSetting

--- AutoClicker.h
+++ AutoClicker.h
@@ -11,12 +11,12 @@
 
 class AutoClicker final : public Module {
 public:
-	const NumberSetting<short> left_cps = NumberSetting<short>("Left-Click CPS", this, 0, 20, 10, [] { return true; });
-	const NumberSetting<short> right_cps = NumberSetting<short>("Right-Click CPS", this, 0, 20, 10, [] { return true; });
-	const NumberSetting<short> random_left = NumberSetting<short>("CPS Random", this, 0, 10, 2, [] { return true; });
-	const NumberSetting<short> random_right = NumberSetting<short>("CPS Random", this, 0, 10, 2, [] { return true; });
+	const NumberSetting<short> left_cps = NumberSetting<short>("Left-Click CPS", this, 0, 20, 10, 1, [] { return true; });
+	const NumberSetting<short> right_cps = NumberSetting<short>("Right-Click CPS", this, 0, 20, 10, 1, [] { return true; });
+	const NumberSetting<short> random_left = NumberSetting<short>("Left CPS Random", this, 0, 10, 2, 1, [] { return true; });
+	const NumberSetting<short> random_right = NumberSetting<short>("Right CPS Random", this, 0, 10, 2, 1, [] { return true; });
 	const BooleanSetting change_rcd_delay = BooleanSetting("Change RCD Ddelay", this, false, [] { return true; });
-	const NumberSetting<short> rcd_delay = NumberSetting<short>("RCD Delay", this, 0, 4, 4, [&] { return change_rcd_delay.value; });
+	const NumberSetting<short> rcd_delay = NumberSetting<short>("RCD Delay", this, 0, 4, 4, 1, [&] { return change_rcd_delay.value; });
 	Clock<std::chrono::milliseconds> clock;
 
 	AutoClicker(const std::string name, const mdl::MODULE_CATEGORY category);
--- NumberSetting.h
+++ NumberSetting.h
@@ -5,10 +5,11 @@ template<class T>
 class NumberSetting final : public Setting<T> {
 public:
 	const T min, max;
+	const T increment;
 
 	/*****************************************************IMPLEMENTATION*****************************************************/
 	// This has to be done this way because of the way C++ handels template classes.
 
-	inline NumberSetting(const std::string name, const Module* parent, const T min, const T max, const T default_value, const std::function<bool(void)> dependency) :
-		min(min), max(max), Setting<T>(name, parent, stg::SETTING_TYPE::NUMBER, default_value, dependency) {}
+	inline NumberSetting(const std::string name, const Module* parent, const T min, const T max, const T increment, const T default_value, const std::function<bool(void)> dependency) :
+		min(min), max(max), increment(increment), Setting<T>(name, parent, stg::SETTING_TYPE::NUMBER, default_value, dependency) {}
 };
