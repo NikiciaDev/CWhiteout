d830da1b811b735ae19f1c6c9ab17c6064a3b09c
NikiciaDev <nikiciayt@gmail.com>
2023-06-26 14:34:54 +0200

    removed timer default keybind starting gui now

--- Timer.cpp
+++ Timer.cpp
@@ -1,6 +1,6 @@
 #include "Timer.h"
 
-Timer::Timer(const std::string name, const mdl::MODULE_CATEGORY category) : Module(name, category, 'G') {}
+Timer::Timer(const std::string name, const mdl::MODULE_CATEGORY category) : Module(name, category, NULL) {}
 
 void Timer::on_enable(std::map<const std::string, JavaClass*>& classes) {
 	jenv_ptr->SetFloatField(classes.find("Timer")->second->instance, *classes.find("Timer")->second->jfields.find("timer_speed")->second, speed.value);
