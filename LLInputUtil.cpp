d08b2389ed116bcd845d1bf6c62880fc03848e5e
NikiciaDev <nikiciayt@gmail.com>
2023-06-27 12:49:02 +0200

    we now only dispatch external keypesses that are in the gui window

--- LLInputUtil.cpp
+++ LLInputUtil.cpp
@@ -4,11 +4,14 @@ namespace liu {
     LRESULT CALLBACK keypress_handler(int nCode, WPARAM wParam, LPARAM lParam) {
         // Check if the event is a keypress event.
         if (nCode == HC_ACTION && wParam == WM_KEYDOWN) {
-            PKBDLLHOOKSTRUCT hs = (PKBDLLHOOKSTRUCT) lParam;
-            unsigned long long c = MapVirtualKey(hs->vkCode, MAPVK_VK_TO_CHAR);
-            if (c != 0) {
-                POINT p{ 0, 0 };
-                whiteout->dispatch_keypress(Key(c, p), !jenv_ptr->CallStaticBooleanMethod(classes.find("Display")->second->jclass, *classes.find("Display")->second->jmethods.find("is_active")->second));
+            const bool ext = !jenv_ptr->CallStaticBooleanMethod(classes.find("Display")->second->jclass, *classes.find("Display")->second->jmethods.find("is_active")->second);
+            if (!ext || whiteout->window.hasFocus()) {
+                PKBDLLHOOKSTRUCT hs = (PKBDLLHOOKSTRUCT) lParam;
+                unsigned long long c = MapVirtualKey(hs->vkCode, MAPVK_VK_TO_CHAR);
+                if (c != 0) {
+                    POINT p{ 0, 0 };
+                    whiteout->dispatch_keypress(Key(c, p), ext);
+                }
             }
         }
         return 0;
@@ -18,12 +21,15 @@ namespace liu {
     // XBUTTON2 is weirdly bugged sometimes.
     LRESULT mousepress_handler(int nCode, WPARAM wParam, LPARAM lParam) {
         if (nCode == HC_ACTION && (wParam == WM_XBUTTONDOWN || wParam == WM_LBUTTONDOWN || wParam == WM_RBUTTONDOWN) ) {
-            PMSLLHOOKSTRUCT hs = (PMSLLHOOKSTRUCT) lParam;
-            if (wParam == WM_RBUTTONDOWN) hs->mouseData = 1;
-            POINT p{ 0, 0 };
-            GetCursorPos(&p);
-            ScreenToClient(whiteout->window.getSystemHandle(), &p);
-            whiteout->dispatch_keypress(Key(hs->mouseData, p), !jenv_ptr->CallStaticBooleanMethod(classes.find("Display")->second->jclass, *classes.find("Display")->second->jmethods.find("is_active")->second));
+            const bool ext = !jenv_ptr->CallStaticBooleanMethod(classes.find("Display")->second->jclass, *classes.find("Display")->second->jmethods.find("is_active")->second);
+            if (!ext || whiteout->window.hasFocus()) {
+                PMSLLHOOKSTRUCT hs = (PMSLLHOOKSTRUCT) lParam;
+                if (wParam == WM_RBUTTONDOWN) hs->mouseData = 1;
+                POINT p{ 0, 0 };
+                GetCursorPos(&p);
+                ScreenToClient(whiteout->window.getSystemHandle(), &p);
+                whiteout->dispatch_keypress(Key(hs->mouseData, p), ext);
+            }
         }
         return 0;
     }
