cd303f5ef4dcb1ed24e5e3dc4c1cb2aab09af657
NikiciaDev <nikiciayt@gmail.com>
2023-06-28 09:00:47 +0200

    mousepress listener now also listens to mousewheel

--- LLInputUtil.cpp
+++ LLInputUtil.cpp
@@ -20,13 +20,15 @@ namespace liu {
     // hs->mouseData = 65536 == XBUTTON1 | hs->mouseData = 131072 == XBUTTON2.
     // XBUTTON2 is weirdly bugged sometimes.
     LRESULT mousepress_handler(int nCode, WPARAM wParam, LPARAM lParam) {
-        if (nCode == HC_ACTION && (wParam == WM_XBUTTONDOWN || wParam == WM_LBUTTONDOWN || wParam == WM_RBUTTONDOWN || wParam == WM_LBUTTONUP) ) {
+        if (nCode == HC_ACTION && (wParam == WM_XBUTTONDOWN || wParam == WM_LBUTTONDOWN || wParam == WM_RBUTTONDOWN || wParam == WM_LBUTTONUP || wParam == WM_MOUSEWHEEL) ) {
             const bool ext = !jenv_ptr->CallStaticBooleanMethod(classes.find("Display")->second->jclass, *classes.find("Display")->second->jmethods.find("is_active")->second);
             if (!ext || whiteout->window.hasFocus()) {
                 PMSLLHOOKSTRUCT hs = (PMSLLHOOKSTRUCT) lParam;
                 if (wParam == WM_LBUTTONDOWN) hs->mouseData = 1;
                 if (wParam == WM_RBUTTONDOWN) hs->mouseData = 2;
                 if (wParam == WM_LBUTTONUP) hs->mouseData = 3;
+                if (hs->mouseData == 4287102976) hs->mouseData = 4;
+                if (hs->mouseData == 7864320) hs->mouseData = 5;
                 POINT p{ 0, 0 };
                 GetCursorPos(&p);
                 ScreenToClient(whiteout->window.getSystemHandle(), &p);
