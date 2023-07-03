8f8e3f384e01335918d356afee2f0f53ffe5ec3c
NikiciaDev <nikiciayt@gmail.com>
2023-07-03 13:48:06 +0200

    implemented drawable string

--- DrawableString.cpp
+++ DrawableString.cpp
@@ -2,7 +2,13 @@
 
 DrawableString::DrawableString(const sf::Vector2f position, const setting::Type type, StringSetting* setting, Whiteout& whiteout) : DrawableSetting(position, type, whiteout), setting(setting) {}
 
-void DrawableString::draw(float& height, sf::Vector2f outline_r_w) { }
+void DrawableString::draw(float& height, sf::Vector2f outline_r_w) {
+	std::string render_s(setting->name);
+	if (!setting->gv<std::string>().empty()) {
+		render_s = setting->gv<std::string>();
+	}
+	font::render(whiteout.window, render_s, pos);
+}
 
 bool DrawableString::on_event(const Key key) {
 	return false;
